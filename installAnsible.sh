acda06fa25b287b6322cac1fc313ebf147cf1c71
Mike Measel <mmeasel@skytap.com>
2019-03-07 07:34:47 -0600

    v1.0
    Ansible files for Skytap Landing Zone

--- installAnsible.sh
+++ installAnsible.sh
@@ -6,7 +6,8 @@ if [ -f /etc/redhat-release ]; then
   sudo yum update
   sudo yum -y install epel-release
   sudo yum -y install ansible
-  sudo yum -y install git
+  sudo yum -y install sshpass
+  #sudo yum -y install git
 fi
 
 if [ -f /etc/lsb-release ]; then
@@ -15,12 +16,22 @@ if [ -f /etc/lsb-release ]; then
   sudo apt-add-repository ppa:ansible/ansible
   sudo apt-get update -y
   sudo apt-get install ansible -y
-  sudo apt-get install git -y
+  sudo apt-get install sshpass -y
+  #sudo apt-get install git -y
 fi
 
 sudo groupadd -g 5001 ansible
 sudo usermod -a -G ansible skytap
 
 ssh-keygen -t rsa -b 4096
+
+read -p "Please insert the PSTools asset in the CD Drive and then press Enter" CD
+sudo mount /dev/cdrom /media/cdrom
+mkdir ansiblebase
+cp -R /media/cdrom/* ansiblebase
+
+
+
+
 
 
